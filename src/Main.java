ac75b19814a27cac707a0bb4487c1bd1375f5d23
drfox81 <drfox11181@mail.ru>
2022-09-23 17:30:09 +0300

    часть 2 задание 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int box = -159;
+        long boxLong = 341414134;
+        short boxShort = 27897;
+        byte boxByte = 67;
+        float x = 27.12f;
+        double y = 2.78641351235;
+        System.out.println("Значение переменной box с типом int равно " + box);
+        System.out.println("Значение переменной boxLong с типом long равно " + boxLong);
+        System.out.println("Значение переменной boxShort с типом short равно " + boxShort);
+        System.out.println("Значение переменной boxByte с типом byte равно " + boxByte);
+        System.out.println("Значение переменной x с типом float равно " + x);
+        System.out.println("Значение переменной y с типом double равно " + y);
+    }
+}
